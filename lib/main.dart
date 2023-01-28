087e52a36087243aa0c29943e2995d6ab78f3e2f
Jushkinbek Bekniyozov <jushqinbekbekniyozov@gmail.com>
2023-01-28 17:01:55 +0500

    firebase added successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -6,7 +7,10 @@ import 'package:shopify/app/const/app_theme.dart';
 import 'app/modules/main/bindings/main_binding.dart';
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
+   
   runApp(
     GetMaterialApp(
       title: "Application",
